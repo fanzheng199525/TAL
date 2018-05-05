9d053f44c6292880228b8ce29c1abeda6083b9e9
fanzheng199525 <37803147+fanzheng199525@users.noreply.github.com>
2018-05-05 11:28:58 +0200

    Add files via upload

--- src/mode2.py
+++ src/mode2.py
@@ -8,8 +8,8 @@ word_tableware = ["fork","knife","spoon","napkin","dish","plate","glass","cup","
 word_music=["piano","organ","flute","guitar","violin","drum","gong","cymbal","harp","cornet"]
 word_animal = ["tiger","rabbit","monkey","cat","dog","bear","wolf","chicken","dug","mouse","rat","horse","sheep"]
 
-family_question = ["Do you think your family is important","Do you go vocation with yor family frequently"]
-fruit_question = ["Do you like eating fruits","What kind of fruits do you like best?"]
+family_question = ["Do you think your family is important?","Do you go vocation with yor family frequently?"]
+fruit_question = ["Do you like eating fruits?","What kind of fruits do you like best?"]
 tableware_question = ["What kind of tablewares do you use the most frequently?","In your country, what kind of tablewares do people use?"]
 animal_question = ["Do you want to have an dometic animal?","Have you had an animal?"]
 music_question = ["Have you learned music?","What kind of musics do you like best?"]
@@ -28,6 +28,7 @@ vocabulary["tableware"]=list_tableware
 vocabulary["music"]=list_music
 vocabulary["animal"]=list_animal
 
+# numero{} is to memorize the sentence used last time
 numero = {}
 numero["family"]=0
 numero["fruit"]=0
@@ -36,6 +37,7 @@ numero["music"]=0
 numero["animal"]=0
 
 def mode2(my_p):
+	# abbreviated sentences: i'm, he's ...
 	cond = re.search(r"(i'm|he's|she's|it's|they're|you're)", my_p, re.I)
 	if cond:
 		myp_list = re.split(r"i'm |he's |she's |it's |they're |you're ", my_p, flags=re.I)
@@ -52,12 +54,14 @@ def mode2(my_p):
 		if re.search(r"(you're)", my_p, re.I):
 			myp_list[0]="am I"
 		print ("Rebot:","Why",myp_list[0],myp_list[1],"?")
-		
+	# no abbreviated sentences: i am, he is...	
 	elif re.search(r"(am |is |was |will be |has been |are |had been |have been |were )", my_p, re.I):
 		myp_list = re.split(r"(am|is|was|will be|has been|are|had been|have been|were)[ ]+", my_p, flags =re.I)
 		if re.search(r"(i )", myp_list[0], re.I):
+			# change the subject
 			myp_list[0]="you"
 			if re.search(r"(am)",myp_list[1], re.I ):
+				# change the verb
 				myp_list[1]="are"
 			elif re.search(r"(was)",myp_list[1], re.I ):
 				myp_list[1]="were"
@@ -72,26 +76,31 @@ def mode2(my_p):
 			elif re.search(r"(have been)",myp_list[1], re.I ):
 				myp_list[1]="has been"
 		else:
+			# Uniform use of "he" for non special subjects  
 			myp_list[0]="he"
 		print("Rebot:","Why",myp_list[1],myp_list[0],myp_list[2],"?")
+	#---------------------------------------------------------------------------------------------------------------------
 	else:
 		flag = 0
 		for key, values in vocabulary.items():
 			for mot in values[0]:
+				# to verifiy there are the keywords in the sentence my_p
 				if re.search(mot, my_p, re.I):
+					# choose a sentence different from last time
 					num = random.randint(0,1)
 					if num==numero[key]:
 						num=(num+1) % 2
 						numero[key]=num
 					else:
 						numero[key]=num
-					print("Rebot: ",values[1][num])
+					print("Rebot:",values[1][num])
 
 					flag = 1
 					break 
 			if flag == 1:
 				break
 		if flag == 0:
+			# Chatbot fail to repond the user in mode 2, it will change to mode 1 automatiquement
 			global_init.minus_mode()
 			print("Rebot:",mode1.rep())
 
